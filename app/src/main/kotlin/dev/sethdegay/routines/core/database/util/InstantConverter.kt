e61812d288a13c5589f1af101fd862cf52ee1200
Seth Degay <158176485+swdegay@users.noreply.github.com>
2025-11-18 10:37:35 +0800

    update visibility modifier

--- app/src/main/kotlin/dev/sethdegay/routines/core/database/util/InstantConverter.kt
+++ app/src/main/kotlin/dev/sethdegay/routines/core/database/util/InstantConverter.kt
@@ -3,7 +3,7 @@ package dev.sethdegay.routines.core.database.util
 import androidx.room.TypeConverter
 import kotlin.time.Instant
 
-class InstantConverter {
+internal class InstantConverter {
     @TypeConverter
     fun longToInstant(value: Long): Instant = Instant.fromEpochSeconds(value)
 
