bb5d8b7ab2826da155ff325cc2a248dea78d47c4
Seth Degay <158176485+swdegay@users.noreply.github.com>
2025-11-19 10:30:11 +0800

    add icon extensions

---
+++ app/src/main/kotlin/dev/sethdegay/routines/core/designsystem/icon/RoutinesIcons.kt
@@ -0,0 +1,8 @@
+package dev.sethdegay.routines.core.designsystem.icon
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+
+object RoutinesIcons {
+    val NavigateUp = Icons.AutoMirrored.Filled.ArrowBack
+}
