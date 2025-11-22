a68d809b7509b6c3a2a2c103ce114bb7c80af68e
Seth Degay <158176485+swdegay@users.noreply.github.com>
2025-11-22 18:21:29 +0800

    add timer controls

--- app/src/main/kotlin/dev/sethdegay/routines/core/designsystem/icon/RoutinesIcons.kt
+++ app/src/main/kotlin/dev/sethdegay/routines/core/designsystem/icon/RoutinesIcons.kt
@@ -2,6 +2,8 @@ package dev.sethdegay.routines.core.designsystem.icon
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.ArrowLeft
+import androidx.compose.material.icons.automirrored.filled.ArrowRight
 import androidx.compose.material.icons.filled.DarkMode
 import androidx.compose.material.icons.filled.LightMode
 import androidx.compose.material.icons.filled.PhoneAndroid
@@ -15,6 +17,8 @@ object RoutinesIcons {
     val LightModeChecked = Icons.Filled.LightMode
     val LightModeUnchecked = Icons.Outlined.LightMode
     val NavigateUp = Icons.AutoMirrored.Filled.ArrowBack
+    val Next = Icons.AutoMirrored.Filled.ArrowRight
+    val Previous = Icons.AutoMirrored.Filled.ArrowLeft
     val SystemChecked = Icons.Filled.PhoneAndroid
     val SystemUnchecked = Icons.Outlined.PhoneAndroid
 }
