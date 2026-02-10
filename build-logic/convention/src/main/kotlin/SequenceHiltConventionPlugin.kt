cdb0cd696123b371e9470bf2b7fa8f49b7290942
Seth Degay <158176485+swdegay@users.noreply.github.com>
2026-02-10 14:49:44 +0800

    refactor code

--- build-logic/convention/src/main/kotlin/SequenceHiltConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/SequenceHiltConventionPlugin.kt
@@ -7,9 +7,19 @@ import org.gradle.kotlin.dsl.dependencies
 
 class SequenceHiltConventionPlugin : Plugin<Project> {
     override fun apply(project: Project) {
+        applyPlugins(project)
+        applyDependencies(project)
+    }
+
+    private fun applyPlugins(project: Project) {
         with(project) {
             apply(plugin = project.pluginId("ksp"))
             apply(plugin = project.pluginId("hilt"))
+        }
+    }
+
+    private fun applyDependencies(project: Project) {
+        with(project) {
             dependencies {
                 "implementation"(library("hilt-android"))
                 "ksp"(library("hilt-android-compiler"))
