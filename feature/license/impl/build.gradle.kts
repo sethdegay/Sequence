d0beb59f83ab99e627481f6f39c4a006bc00d8fd
Seth Degay <mail@sethdegay.dev>
2026-06-04 20:51:11 +0800

    add material3 api opt in

--- feature/license/impl/build.gradle.kts
+++ feature/license/impl/build.gradle.kts
@@ -6,6 +6,10 @@ android {
     namespace = "dev.sethdegay.sequence.feature.license.impl"
 }
 
+kotlin {
+    compilerOptions { optIn.add("androidx.compose.material3.ExperimentalMaterial3Api") }
+}
+
 dependencies {
     implementation(projects.feature.license.api)
     implementation(libs.aboutlibraries.compose.m3)
