cd02fbd3ef2a677ba65c87003b2faa14bef13252
Seth Degay <mail@sethdegay.dev>
2026-08-13 21:15:57 +0800

    vertically center switch

--- core/ui/src/main/kotlin/dev/sethdegay/sequence/core/ui/BooleanPreference.kt
+++ core/ui/src/main/kotlin/dev/sethdegay/sequence/core/ui/BooleanPreference.kt
@@ -17,6 +17,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.tooling.preview.Preview
@@ -67,6 +68,7 @@ fun BooleanPreference(
             supportingColor = CardDefaults.cardColors().contentColor,
         ),
         content = { Text(title) },
+        verticalAlignment = Alignment.CenterVertically,
     )
 }
 
